9454d53fc09d769e6eea1a1f1f52637414950be1
almuhdilkarim <al.muhdil.0@gmail.com>
2023-11-24 18:51:26 +0700

    falcon dev version 1

---
+++ assets/reblock/blocks/darkmode/head/main.js
@@ -0,0 +1,22 @@
+(() => {
+
+    let get_dark_mode = localStorage.getItem('darkmode');
+
+    let get_hour_nows = new Date().getHours();
+    let get_days_nows = get_hour_nows >= 6 && get_hour_nows < 18;
+
+    if ( get_dark_mode === 'dark'  ) {
+        document.documentElement.dataset.mode = 'dark';
+    }
+    else if ( get_dark_mode === 'light'  ) {
+        document.documentElement.dataset.mode = 'light';
+    }
+    else if ( window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches || get_days_nows == false ) {
+        document.documentElement.dataset.mode = 'dark';
+    }
+    else {
+        document.documentElement.dataset.mode = 'light';
+    }
+
+})();
+
