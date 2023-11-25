9bd5a0bd2cc64dd5db275732ba55ce5fa952c235
syslectore <karim@katana.katana.loc>
2023-11-25 14:12:08 +0700

    falcon dev version 1

--- assets/reblock/blocks/darkmode/foot/main.js
+++ assets/reblock/blocks/darkmode/foot/main.js
@@ -8,15 +8,23 @@
         let dark_mode_target = dark_mode_opened.querySelector('.menu');
         let dark_mode_action = Array.prototype.slice.call( dark_mode_target.querySelectorAll('.item') );
     
-        dark_mode_opened.addEventListener('click', (event) => {
-            dark_mode_target.classList.toggle('d-hide');
+        dark_mode_opened.addEventListener('mouseenter', () => {
+            if ( dark_mode_target.classList.contains('d-hide' ) ) {
+                dark_mode_target.classList.remove('d-hide');
+            }
+            dark_mode_target.dataset.dropdown = "open";
+        });
+
+        dark_mode_opened.addEventListener('mouseleave', () => {
+            dark_mode_target.dataset.dropdown = "close";
         });
     
         dark_mode_action.map((event)=> {
             event.addEventListener( 'click', (event) => {
                 let value = event.currentTarget.dataset.themeset;
-                document.documentElement.dataset.mode = value;
+                dark_mode_target.dataset.dropdown = "close";
                 localStorage.setItem('darkmode', value );
+                document.documentElement.dataset.mode = value;
             });
         })
     });
